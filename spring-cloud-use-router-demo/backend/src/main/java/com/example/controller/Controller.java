c6190ef01b7207fd45346078a2aff11ee5f02022
jsonwan <jsonwan@tencent.com>
2025-05-15 09:59:06 +0800

    spring-cloud-use-router-demo project: add virtual service and v1/v2 deployment for backend

--- spring-cloud-use-router-demo/backend/src/main/java/com/example/controller/Controller.java
+++ spring-cloud-use-router-demo/backend/src/main/java/com/example/controller/Controller.java
@@ -1,6 +1,7 @@
 
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @Value("${version:v0}")
+    private String version;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello " + version;
     }
 }
